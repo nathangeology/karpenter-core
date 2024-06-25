206215a15cb4ccdf1f12281e57df4fd6acd05f9d
nathangeology <jonesflp@amazon.com>
2024-06-25 13:36:51 -0700

    Made a new sim test with debugging so I can adjust the pod deployments for the scenario.

--- test/suites/perf/scheduling_test.go
+++ test/suites/perf/scheduling_test.go
@@ -17,16 +17,13 @@ limitations under the License.
 package perf_test
 
 import (
+	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/samber/lo"
 	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"sigs.k8s.io/karpenter/pkg/apis/v1beta1"
@@ -37,62 +34,121 @@ var replicas int = 100
 
 var _ = Describe("Performance", func() {
 	Context("Provisioning", func() {
-		It("should do simple provisioning", func() {
-			deployment := test.Deployment(test.DeploymentOptions{
-				Replicas: int32(replicas),
-				PodOptions: test.PodOptions{
-					ObjectMeta: metav1.ObjectMeta{
-						Labels: testLabels,
-					},
-					ResourceRequirements: v1.ResourceRequirements{
-						Requests: v1.ResourceList{
-							v1.ResourceCPU: resource.MustParse("1"),
-						},
-					},
-				}})
-			env.ExpectCreated(deployment)
-			env.ExpectCreated(nodePool, nodeClass)
-			env.EventuallyExpectHealthyPodCount(labelSelector, replicas)
-		})
-		It("should do simple provisioning and simple drift", func() {
-			deployment := test.Deployment(test.DeploymentOptions{
-				Replicas: int32(replicas),
-				PodOptions: test.PodOptions{
-					ObjectMeta: metav1.ObjectMeta{
-						Labels: testLabels,
-					},
-					ResourceRequirements: v1.ResourceRequirements{
-						Requests: v1.ResourceList{
-							v1.ResourceCPU: resource.MustParse("1"),
-						},
-					},
-				}})
-			env.ExpectCreated(deployment)
-			env.ExpectCreated(nodePool, nodeClass)
-			env.EventuallyExpectHealthyPodCount(labelSelector, replicas)
-
-			env.TimeIntervalCollector.Start("Drift")
-			nodePool.Spec.Template.ObjectMeta.Labels = lo.Assign(nodePool.Spec.Template.ObjectMeta.Labels, map[string]string{
-				"test-drift": "true",
-			})
-			env.ExpectUpdated(nodePool)
-			// Eventually expect one node to be drifted
-			Eventually(func(g Gomega) {
-				nodeClaims := &v1beta1.NodeClaimList{}
-				g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1beta1.ConditionTypeDrifted})).To(Succeed())
-				g.Expect(len(nodeClaims.Items)).ToNot(Equal(0))
-			}).WithTimeout(5 * time.Second).Should(Succeed())
-			// Then eventually expect no nodes to be drifted
-			Eventually(func(g Gomega) {
-				nodeClaims := &v1beta1.NodeClaimList{}
-				g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1beta1.ConditionTypeDrifted})).To(Succeed())
-				g.Expect(len(nodeClaims.Items)).To(Equal(0))
-			}).WithTimeout(300 * time.Second).Should(Succeed())
-			env.TimeIntervalCollector.End("Drift")
-		})
-		It("should do complex provisioning", func() {
+		//It("should do simple provisioning", func() {
+		//	deployment := test.Deployment(test.DeploymentOptions{
+		//		Replicas: int32(replicas),
+		//		PodOptions: test.PodOptions{
+		//			ObjectMeta: metav1.ObjectMeta{
+		//				Labels: testLabels,
+		//			},
+		//			ResourceRequirements: v1.ResourceRequirements{
+		//				Requests: v1.ResourceList{
+		//					v1.ResourceCPU: resource.MustParse("1"),
+		//				},
+		//			},
+		//		}})
+		//	env.ExpectCreated(deployment)
+		//	env.ExpectCreated(nodePool, nodeClass)
+		//	env.EventuallyExpectHealthyPodCount(labelSelector, replicas)
+		//})
+		//It("should do simple provisioning and simple drift", func() {
+		//	deployment := test.Deployment(test.DeploymentOptions{
+		//		Replicas: int32(replicas),
+		//		PodOptions: test.PodOptions{
+		//			ObjectMeta: metav1.ObjectMeta{
+		//				Labels: testLabels,
+		//			},
+		//			ResourceRequirements: v1.ResourceRequirements{
+		//				Requests: v1.ResourceList{
+		//					v1.ResourceCPU: resource.MustParse("1"),
+		//				},
+		//			},
+		//		}})
+		//	env.ExpectCreated(deployment)
+		//	env.ExpectCreated(nodePool, nodeClass)
+		//	env.EventuallyExpectHealthyPodCount(labelSelector, replicas)
+		//
+		//	env.TimeIntervalCollector.Start("Drift")
+		//	nodePool.Spec.Template.ObjectMeta.Labels = lo.Assign(nodePool.Spec.Template.ObjectMeta.Labels, map[string]string{
+		//		"test-drift": "true",
+		//	})
+		//	env.ExpectUpdated(nodePool)
+		//	// Eventually expect one node to be drifted
+		//	Eventually(func(g Gomega) {
+		//		nodeClaims := &v1beta1.NodeClaimList{}
+		//		g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1beta1.ConditionTypeDrifted})).To(Succeed())
+		//		g.Expect(len(nodeClaims.Items)).ToNot(Equal(0))
+		//	}).WithTimeout(5 * time.Second).Should(Succeed())
+		//	// Then eventually expect no nodes to be drifted
+		//	Eventually(func(g Gomega) {
+		//		nodeClaims := &v1beta1.NodeClaimList{}
+		//		g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1beta1.ConditionTypeDrifted})).To(Succeed())
+		//		g.Expect(len(nodeClaims.Items)).To(Equal(0))
+		//	}).WithTimeout(300 * time.Second).Should(Succeed())
+		//	env.TimeIntervalCollector.End("Drift")
+		//})
+		//It("should do complex provisioning", func() {
+		//	deployments := []*appsv1.Deployment{}
+		//	podOptions := test.MakeDiversePodOptions()
+		//	for _, option := range podOptions {
+		//		deployments = append(deployments, test.Deployment(
+		//			test.DeploymentOptions{
+		//				PodOptions: option,
+		//				Replicas:   int32(replicas / len(podOptions)),
+		//			},
+		//		))
+		//	}
+		//	for _, dep := range deployments {
+		//		env.ExpectCreated(dep)
+		//	}
+		//	env.TimeIntervalCollector.Start("PostDeployment")
+		//	env.ExpectCreated(nodePool, nodeClass)
+		//	env.EventuallyExpectHealthyPodCountWithTimeout(10*time.Minute, labelSelector, len(deployments)*replicas)
+		//	env.TimeIntervalCollector.End("PostDeployment")
+		//})
+		//It("should do complex provisioning and complex drift", func() {
+		//	deployments := []*appsv1.Deployment{}
+		//	podOptions := test.MakeDiversePodOptions()
+		//	for _, option := range podOptions {
+		//		deployments = append(deployments, test.Deployment(
+		//			test.DeploymentOptions{
+		//				PodOptions: option,
+		//				Replicas:   int32(replicas / len(podOptions)),
+		//			},
+		//		))
+		//	}
+		//	for _, dep := range deployments {
+		//		env.ExpectCreated(dep)
+		//	}
+		//
+		//	env.ExpectCreated(nodePool, nodeClass)
+		//	env.EventuallyExpectHealthyPodCountWithTimeout(10*time.Minute, labelSelector, len(deployments)*replicas)
+		//
+		//	env.TimeIntervalCollector.Start("Drift")
+		//	nodePool.Spec.Template.ObjectMeta.Labels = lo.Assign(nodePool.Spec.Template.ObjectMeta.Labels, map[string]string{
+		//		"test-drift": "true",
+		//	})
+		//	env.ExpectUpdated(nodePool)
+		//	// Eventually expect one node to be drifted
+		//	Eventually(func(g Gomega) {
+		//		nodeClaims := &v1beta1.NodeClaimList{}
+		//		g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1beta1.ConditionTypeDrifted})).To(Succeed())
+		//		g.Expect(len(nodeClaims.Items)).ToNot(Equal(0))
+		//	}).WithTimeout(5 * time.Second).Should(Succeed())
+		//	// Then eventually expect no nodes to be drifted
+		//	Eventually(func(g Gomega) {
+		//		nodeClaims := &v1beta1.NodeClaimList{}
+		//		g.Expect(env.Client.List(env, nodeClaims, client.MatchingFields{"status.conditions[*].type": v1beta1.ConditionTypeDrifted})).To(Succeed())
+		//		g.Expect(len(nodeClaims.Items)).To(Equal(0))
+		//	}).WithTimeout(10 * time.Minute).Should(Succeed())
+		//	env.TimeIntervalCollector.End("Drift")
+		//})
+		It("should do staggered multi-deployment provisioning and drift", func() {
 			deployments := []*appsv1.Deployment{}
+			// TODO: Adjust pod options to be a fixed set of option (maybe update the ones I get from the k8s test api)
+			fmt.Printf("Debug printing of pod options so I can make adjustments:\n")
 			podOptions := test.MakeDiversePodOptions()
+			fmt.Printf("%#v\n", podOptions)
 			for _, option := range podOptions {
 				deployments = append(deployments, test.Deployment(
 					test.DeploymentOptions{
@@ -102,25 +158,7 @@ var _ = Describe("Performance", func() {
 				))
 			}
 			for _, dep := range deployments {
-				env.ExpectCreated(dep)
-			}
-			env.TimeIntervalCollector.Start("PostDeployment")
-			env.ExpectCreated(nodePool, nodeClass)
-			env.EventuallyExpectHealthyPodCountWithTimeout(10*time.Minute, labelSelector, len(deployments)*replicas)
-			env.TimeIntervalCollector.End("PostDeployment")
-		})
-		It("should do complex provisioning and complex drift", func() {
-			deployments := []*appsv1.Deployment{}
-			podOptions := test.MakeDiversePodOptions()
-			for _, option := range podOptions {
-				deployments = append(deployments, test.Deployment(
-					test.DeploymentOptions{
-						PodOptions: option,
-						Replicas:   int32(replicas / len(podOptions)),
-					},
-				))
-			}
-			for _, dep := range deployments {
+				time.Sleep(2 * time.Second)
 				env.ExpectCreated(dep)
 			}
 
