31e983a02672514db2cb59460f5abe9ffb07cb63
nathangeology <jonesflp@amazon.com>
2024-07-03 13:55:35 -0600

    getting invalid image name error.

--- test/suites/perf/local_cluster_setup.sh
+++ test/suites/perf/local_cluster_setup.sh
@@ -4,6 +4,24 @@ kind create cluster --name ${context_name}
 cd ..
 cd ..
 cd ..
+
+
+kubectl create namespace "$prom_ns"
+helm upgrade --values valuesv3.yaml --install prometheus prometheus-community/kube-prometheus-stack -n "$prom_ns" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[0].targetLabel=metrics_path" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[0].action=replace" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[0].sourceLabels[0]=__metrics_path__" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[1].targetLabel=clusterName" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[1].replacement=$CLUSTER_NAME" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[2].targetLabel=gitRef" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[2].replacement=$(git rev-parse HEAD)" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[3].targetLabel=mostRecentTag" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[3].replacement=$(git describe --abbrev=0 --tags)" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[4].targetLabel=commitsAfterTag" \
+  --set "kubelet.serviceMonitor.cAdvisorRelabelings[4].replacement=\"$(git describe --tags | cut -d '-' -f 2)\"" \
+  --wait
+export KWOK_REPO=kind.local
+export KIND_CLUSTER_NAME=chart-testing
 make apply-with-kind
 cat <<EOF | envsubst | kubectl apply -f -
 apiVersion: karpenter.sh/v1beta1
@@ -31,26 +49,14 @@ spec:
     consolidationPolicy: WhenUnderutilized
     expireAfter: 720h # 30 * 24h = 720h
 EOF
-kubectl create namespace "$prom_ns"
-helm upgrade --values valuesv3.yaml --install prometheus prometheus-community/kube-prometheus-stack -n "$prom_ns" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[0].targetLabel=metrics_path" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[0].action=replace" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[0].sourceLabels[0]=__metrics_path__" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[1].targetLabel=clusterName" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[1].replacement=$CLUSTER_NAME" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[2].targetLabel=gitRef" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[2].replacement=$(git rev-parse HEAD)" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[3].targetLabel=mostRecentTag" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[3].replacement=$(git describe --abbrev=0 --tags)" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[4].targetLabel=commitsAfterTag" \
-  --set "kubelet.serviceMonitor.cAdvisorRelabelings[4].replacement=\"$(git describe --tags | cut -d '-' -f 2)\"" \
-  --wait
 # Testing out pyroscope for profiling
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 # This only creates one replica for testing, but not multiple backends
 #kubectl create namespace pyroscope-test
-helm -n karpenter install pyroscope grafana/pyroscope
-make test
+helm -n "$prom_ns" install pyroscope grafana/pyroscope
+OUTPUT_DIR=$(mktemp -d)
+export OUTPUT_DIR
+make e2etests
 read -p "press enter to delete cluster" temp_var
 kind delete cluster --name ${context_name}
