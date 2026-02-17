c11c88007a6ee06280e61f8357bce8137ed211d4
Nathaniel Jones <jonesflp@amazon.com>
2026-02-17 14:24:47 -0800

    setting default to random

--- test/suites/performance/suite_test.go
+++ test/suites/performance/suite_test.go
@@ -58,7 +58,7 @@ var (
 		if val := os.Getenv("POD_DELETION_COST_RANKING_STRATEGY"); val != "" {
 			return val
 		}
-		return "Unknown" // Default for performance tests
+		return "Random" // Default for performance tests
 	}()
 
 	// podDeletionCostChangeDetection controls whether change detection optimization is enabled
